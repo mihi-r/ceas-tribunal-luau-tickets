7e50f77b67ed4c5138690e05d1efb1be84c45cfd
lindsaln <lindsaln@mail.uc.edu>
2019-06-09 13:24:00 -0400

    #14 Automatically adds / in birthday for Safari Users

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
-console.log("Test");
+var birthdayInput = document.querySelector(".selection-intro #birthDate");
+
+birthdayInput.onkeydown = function (event) {
+    if ((birthdayInput.value.length == 2 || birthdayInput.value.length == 5) && birthdayInput.value != "") {
+        birthdayInput.value+="/";
+    }
+}
