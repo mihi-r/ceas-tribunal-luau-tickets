74a5026a26af9b41a0c7f85317adf29fa0357dee
patel3mh <patel3mh@mail.uc.edu>
2019-06-09 22:49:29 -0400

    Added Venmo deep link

--- scripts/info-view.js
+++ scripts/info-view.js
@@ -1,14 +1,38 @@
 /**
+ * Displays information about the Venmo transaction and generates the Venmo deep link.
+ * @param {int} price - The price of the ticket to display.
+ * @param {string} venmoRecipient - The reciepient of the venmo transaction to display.
+ */
+const generateVenmoInfo = function generateVenmoInfoForView(price, venmoRecipient) {
+  const venmoLinkButton = document.querySelector('.intro .open-venmo');
+  const venmoLinkPath = `venmo://paycharge?txn=pay&recipients=${venmoRecipient}&amount=${price}&note=Luau Ticket`;
+
+  venmoLinkButton.onclick = function openVenmoOnClick() {
+    if (/Android|webOS|iPhone|iPad|iPod|Opera Mini/i.test(navigator.userAgent)) {
+      window.open(venmoLinkPath, '_blank');
+    } else {
+      window.open('https://venmo.com', '_blank');
+    }
+  };
+
+  const DOMTicketPrice = document.querySelector('.intro .ticket-price');
+  const DOMVenmoRecipient = document.querySelector('.intro .venmo-recipient');
+
+  DOMTicketPrice.textContent = price;
+  DOMVenmoRecipient.textContent = venmoRecipient;
+};
+
+/**
  * Displays various information about the event details in the form.
  * @param {string} description - The description of the event to display.
  * @param {int} price - The price of the ticket to display.
  * @param {string} venmoRecipient - The reciepient of the venmo transaction to display.
  */
 const displayInfo = function displayInfoForView(description, price, venmoRecipient) {
-  const descriptionDiv = document.querySelector('.selection-intro .intro-info');
+  const descriptionDiv = document.querySelector('.intro .intro-info');
   descriptionDiv.textContent = description;
 
-  // TODO: Add price and venmo recipient information
+  generateVenmoInfo(price, venmoRecipient);
 };
 
 /**
